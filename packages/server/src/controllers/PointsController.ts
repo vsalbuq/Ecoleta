2a534ecf00992f6d7098c9f4a2bf4874e0e8fa6f
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2020-06-10 20:48:28 -0300

    feat: image upload

--- packages/server/src/controllers/PointsController.ts
+++ packages/server/src/controllers/PointsController.ts
@@ -62,12 +62,15 @@ class PointsController {
 
       const point_id = insertedIds[0];
 
-      const pointItems = items.map((item_id: number) => {
-        return {
-          item_id,
-          point_id,
-        };
-      });
+      const pointItems = items
+        .split(',')
+        .map((item: string) => Number(item.trim()))
+        .map((item_id: number) => {
+          return {
+            item_id,
+            point_id,
+          };
+        });
 
       await trx('points_items').insert(pointItems);
 
